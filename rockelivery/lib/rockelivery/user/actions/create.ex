4b30bab4403f847bb1d2a67dac11697b7ea79433
joabehenrique <henriquecidoz@hotmail.com>
2022-03-13 21:53:29 -0300

    feat: adicionado handle para :error/:ok no insert

--- rockelivery/lib/rockelivery/user/actions/create.ex
+++ rockelivery/lib/rockelivery/user/actions/create.ex
@@ -6,5 +6,11 @@ defmodule Rockelivery.User.Actions.Create do
     params
     |> User.changeset()
     |> Repo.insert()
+    |> handle_insert()
   end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}),
+    do: {:error, %{status: :bad_request, result: result}}
 end
