448f79b94f1f725e9754143b415cb6ec8cc3786e
joabehenrique <henriquecidoz@hotmail.com>
2022-03-13 21:54:42 -0300

    feat: adicionado create user

--- rockelivery/lib/rockelivery_web/controllers/user_controller.ex
+++ rockelivery/lib/rockelivery_web/controllers/user_controller.ex
@@ -1,7 +1,15 @@
 defmodule RockeliveryWeb.UserController do
   use RockeliveryWeb, :controller
+  alias Rockelivery.User.User
+  alias RockeliveryWeb.FallbackController
+
+  action_fallback FallbackController
 
   def create(conn, params) do
-    Rockelivery.create_user(params)
+    with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create_user.json", user: user)
+    end
   end
 end
