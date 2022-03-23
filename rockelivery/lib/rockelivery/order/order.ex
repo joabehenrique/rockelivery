5bdeb5fe6fc7ffec06ec768505651204ea30de84
joabehenrique <henriquecidoz@hotmail.com>
2022-03-23 14:17:50 -0300

    feat: created order schema

---
+++ rockelivery/lib/rockelivery/order/order.ex
@@ -0,0 +1,34 @@
+defmodule Rockelivery.Order.Order do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Rockelivery.Item.Item
+  alias Rockelivery.User.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  @required_params [:address, :comments, :payment_method, :user_id]
+  @payment_categories [:money, :credit_card, :debit_card]
+  @derive {Jason.Encoder, only: [:id, :address, :comments, :payment_method, :user_id, :items]}
+
+  schema "orders" do
+    field(:address, :string)
+    field(:comments, :string)
+    field(:payment_method, Ecto.Enum, values: @payment_categories)
+
+    many_to_many(:items, Item, join_through: "orders_items")
+    belongs_to(:user, User)
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params, items) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> put_assoc(:items, items)
+    |> validate_length(:address, min: 10)
+    |> validate_length(:comments, min: 6)
+  end
+end
