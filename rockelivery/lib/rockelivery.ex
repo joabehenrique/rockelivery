3ee3f21cc945e5c2c71dad50da19a8a3421b1ae8
joabehenrique <henriquecidoz@hotmail.com>
2022-03-23 14:16:31 -0300

    feat: added delegate of create item and order

--- rockelivery/lib/rockelivery.ex
+++ rockelivery/lib/rockelivery.ex
@@ -1,4 +1,8 @@
 defmodule Rockelivery do
+  alias Rockelivery.Item.Actions.Create, as: ItemCreate
+
+  alias Rockelivery.Order.Actions.Create, as: OrderCreate
+
   alias Rockelivery.User.Actions.Create, as: UserCreate
   alias Rockelivery.User.Actions.Delete, as: UserDelete
   alias Rockelivery.User.Actions.Find, as: UserFind
@@ -8,4 +12,8 @@ defmodule Rockelivery do
   defdelegate find_user_by_id(params), to: UserFind, as: :call
   defdelegate delete_user(params), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
+
+  defdelegate create_item(params), to: ItemCreate, as: :call
+
+  defdelegate create_order(params), to: OrderCreate, as: :call
 end
