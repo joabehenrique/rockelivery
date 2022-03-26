fcf2bb544ca8afb1b9d6c4b8f288295332407873
joabehenrique <henriquecidoz@hotmail.com>
2022-03-25 22:19:16 -0300

    test: config profile for mox behaviour

--- rockelivery/config/test.exs
+++ rockelivery/config/test.exs
@@ -13,6 +13,9 @@ config :rockelivery, Rockelivery.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :rockelivery, Rockelivery.User.Actions.Create,
+  via_cep_adapter: Rockelivery.ViaCep.ClientMock
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rockelivery, RockeliveryWeb.Endpoint,
