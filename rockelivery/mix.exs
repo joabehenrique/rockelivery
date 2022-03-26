b4f1143007dbe2ae57fc18e22411e45bc1558203
joabehenrique <henriquecidoz@hotmail.com>
2022-03-26 19:20:56 -0300

    build: added guardian dependency

--- rockelivery/mix.exs
+++ rockelivery/mix.exs
@@ -57,7 +57,8 @@ defmodule Rockelivery.MixProject do
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.17"},
       {:bypass, "~> 2.1", only: :test},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+      {:guardian, "~> 2.0"}
     ]
   end
 
