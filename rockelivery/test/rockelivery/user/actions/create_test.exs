58e2611efe94677df2fa9ca13a6e84fd70e08f90
joabehenrique <henriquecidoz@hotmail.com>
2022-03-25 22:24:41 -0300

    test: added client mock

--- rockelivery/test/rockelivery/user/actions/create_test.exs
+++ rockelivery/test/rockelivery/user/actions/create_test.exs
@@ -1,13 +1,33 @@
 defmodule Rockelivery.User.CreateTest do
-  use Rockelivery.DataCase
+  use Rockelivery.DataCase, async: true
+
+  import Mox
   import Rockelivery.Factory
+
   alias Rockelivery.Error
-  alias Rockelivery.User.User
   alias Rockelivery.User.Actions.Create
+  alias Rockelivery.User.User
+  alias Rockelivery.ViaCep.ClientMock
 
   describe "call/1" do
     test "when all params is valid, return the user" do
       params = build(:user_params)
+
+      expect(ClientMock, :get_cep_info, fn _cep ->
+        {:ok,
+         %{
+           "bairro" => "Sé",
+           "cep" => "01001-000",
+           "complemento" => "lado ímpar",
+           "ddd" => "11",
+           "gia" => "1004",
+           "ibge" => "3550308",
+           "localidade" => "São Paulo",
+           "logradouro" => "Praça da Sé",
+           "siafi" => "7107",
+           "uf" => "SP"
+         }}
+      end)
 
       response = Create.call(params)
 
@@ -15,7 +35,7 @@ defmodule Rockelivery.User.CreateTest do
     end
 
     test "when there are invalid params, returns an error" do
-      params = build(:user_params, %{age: 12, cpf: "123456789"})
+      params = build(:user_params, %{"age" => 12, "cpf" => "123456789"})
 
       response = Create.call(params)
 
