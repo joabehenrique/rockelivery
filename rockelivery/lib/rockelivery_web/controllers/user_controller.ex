4d9800ed17e24a020129607d54b4ede8ee6410ca
joabehenrique <henriquecidoz@hotmail.com>
2022-03-26 19:24:07 -0300

    refactor: added authentication

--- rockelivery/lib/rockelivery_web/controllers/user_controller.ex
+++ rockelivery/lib/rockelivery_web/controllers/user_controller.ex
@@ -1,15 +1,18 @@
 defmodule RockeliveryWeb.UserController do
   use RockeliveryWeb, :controller
+
   alias Rockelivery.User.User
+  alias RockeliveryWeb.Auth.Guardian
   alias RockeliveryWeb.FallbackController
 
-  action_fallback FallbackController
+  action_fallback(FallbackController)
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Rockelivery.create_user(params) do
+    with {:ok, %User{} = user} <- Rockelivery.create_user(params),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
       conn
       |> put_status(:created)
-      |> render("create.json", user: user)
+      |> render("create.json", user: user, token: token)
     end
   end
 
@@ -18,6 +21,14 @@ defmodule RockeliveryWeb.UserController do
       conn
       |> put_status(:ok)
       |> render("show.json", user: user)
+    end
+  end
+
+  def sign_in(conn, params) do
+    with {:ok, token} <- Guardian.authenticate(params) do
+      conn
+      |> put_status(:ok)
+      |> render("sign_in.json", token: token)
     end
   end
 
