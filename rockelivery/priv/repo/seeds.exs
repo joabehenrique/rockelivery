0347f43077531644f62cab9f6b5f42fca9661f97
joabehenrique <henriquecidoz@hotmail.com>
2022-03-29 23:13:52 -0300

    feat: added seed for db

--- rockelivery/priv/repo/seeds.exs
+++ rockelivery/priv/repo/seeds.exs
@@ -9,3 +9,48 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Rockelivery.User.User
+alias Rockelivery.Item.Item
+alias Rockelivery.Order.Order
+alias Rockelivery.Repo
+
+IO.puts("======Inserting user...======")
+
+user = %User{
+  age: 27,
+  address: "Rua paulista, 15",
+  cep: "12345678",
+  cpf: "12345678900",
+  email: "henriqueciido@hotmail.com",
+  password: "123456",
+  name: "Joabe Henrique"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1 = %Item{
+  category: :food,
+  description: "banana frita",
+  price: Decimal.new("15.50"),
+  photo: "priv/photos/banana_frita.png"
+}
+
+item2 = %Item{
+  category: :food,
+  description: "banana cozida",
+  price: Decimal.new("10.50"),
+  photo: "priv/photos/banana_cozida.png"
+}
+
+Repo.insert!(item1)
+Repo.insert!(item2)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item1, item2],
+  address: "Rua paulista, 15",
+  comments: "sem canela",
+  payment_method: :money
+}
+
+Repo.insert!(order)
